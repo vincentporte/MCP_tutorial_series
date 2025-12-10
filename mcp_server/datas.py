d887a42674205908fb3c2bd30cf160a973c8cc62
vincent porte <vincent@neuralia.co>
2025-12-10 18:44:04 +0100

    add datas to play with in tutoriels

--- mcp_server/datas.py
+++ mcp_server/datas.py
@@ -3,42 +3,42 @@ from enum import Enum
 from typing import TypedDict
 
 
-PersonId = str
-PickUpPointId = str
-ShippingId = str
+UserId = str
+PickupId = str
+ShipmentId = str
 
 
-class Person(TypedDict):
-    name: str
-    email: str
-    postal_code: str
-
-
-class PickUpPoint(TypedDict):
-    postal_code: str
-    available_capacity: int
-
-
-class Shipping(TypedDict):
-    sender_id: PersonId
-    deposit_pickup_point_id: PickUpPointId | None
-    recipient_id: PersonId
-    withdrawal_pickup_point_id: PickUpPointId | None
-    status: ShippingStatus
-    updated_at: datetime
-
-
-class ShippingStatus(Enum):
+class ShipmentStatus(Enum):
     AWAITING_DEPOSIT = 0
-    PENDING_PICKUP = 1
+    AWAITING_PICKUP = 1
     IN_TRANSIT = 2
-    PENDING_WITHDRAW = 3
+    AWAITING_WITHDRAW = 3
     WITHDRAWN = 4
     CANCELLED = 5
     RETURNED = 6
 
 
-PEOPLE_TABLE: dict[PersonId, Person] = {
+class User(TypedDict):
+    name: str
+    email: str
+    postal_code: str
+
+
+class Pickup(TypedDict):
+    postal_code: str
+    available_capacity: int
+
+
+class Shipment(TypedDict):
+    sender_id: UserId
+    deposit_pickup_id: PickupId | None
+    recipient_id: UserId
+    withdrawal_pickup_id: PickupId | None
+    status: ShipmentStatus
+    updated_at: datetime
+
+
+USER_TABLE: dict[UserId, User] = {
     "pe01": {
         "name": "Alice Johnson",
         "email": "alice@fastshipping.com",
@@ -56,7 +56,7 @@ PEOPLE_TABLE: dict[PersonId, Person] = {
     },
 }
 
-PICKUP_POINT_TABLE: dict[PickUpPointId, PickUpPoint] = {
+PICKUP_TABLE: dict[PickupId, Pickup] = {
     "pp01": {
         "postal_code": "FR-14000",
         "available_capacity": 5,
@@ -79,53 +79,53 @@ PICKUP_POINT_TABLE: dict[PickUpPointId, PickUpPoint] = {
     },
 }
 
-SHIPPING_TABLE: dict[ShippingId, Shipping] = {
+SHIPMENT_TABLE: dict[ShipmentId, Shipment] = {
     "sh01": {
         "sender_id": "pe03",
-        "deposit_pickup_point_id": "pp03",
+        "deposit_pickup_id": "pp03",
         "recipient_id": "pe01",
-        "withdrawal_pickup_point_id": "pp01",
-        "status": ShippingStatus.WITHDRAWN,
+        "withdrawal_pickup_id": "pp01",
+        "status": ShipmentStatus.WITHDRAWN,
         "updated_at": datetime.fromisoformat("2025-12-01T12:00:00"),
     },
     "sh02": {
         "sender_id": "pe02",
-        "deposit_pickup_point_id": "pp04",
+        "deposit_pickup_id": "pp04",
         "recipient_id": "pe03",
-        "withdrawal_pickup_point_id": "pp03",
-        "status": ShippingStatus.PENDING_WITHDRAW,
+        "withdrawal_pickup_id": "pp03",
+        "status": ShipmentStatus.AWAITING_WITHDRAW,
         "updated_at": datetime.fromisoformat("2025-12-02T10:00:00"),
     },
     "sh03": {
         "sender_id": "pe01",
-        "deposit_pickup_point_id": "pp01",
+        "deposit_pickup_id": "pp01",
         "recipient_id": "pe02",
-        "withdrawal_pickup_point_id": "pp02",
-        "status": ShippingStatus.IN_TRANSIT,
+        "withdrawal_pickup_id": "pp02",
+        "status": ShipmentStatus.IN_TRANSIT,
         "updated_at": datetime.fromisoformat("2025-12-03T14:00:00"),
     },
     "sh04": {
         "sender_id": "pe01",
-        "deposit_pickup_point_id": "pp01",
+        "deposit_pickup_id": "pp01",
         "recipient_id": "pe03",
-        "withdrawal_pickup_point_id": "pp03",
-        "status": ShippingStatus.AWAITING_DEPOSIT,
+        "withdrawal_pickup_id": "pp03",
+        "status": ShipmentStatus.AWAITING_DEPOSIT,
         "updated_at": datetime.fromisoformat("2025-12-04T15:00:00"),
     },
     "sh05": {
         "sender_id": "pe03",
-        "deposit_pickup_point_id": "pp03",
+        "deposit_pickup_id": "pp03",
         "recipient_id": "pe02",
-        "withdrawal_pickup_point_id": "pp02",
-        "status": ShippingStatus.IN_TRANSIT,
+        "withdrawal_pickup_id": "pp02",
+        "status": ShipmentStatus.IN_TRANSIT,
         "updated_at": datetime.fromisoformat("2025-12-05T15:00:00"),
     },
     "sh06": {
         "sender_id": "pe03",
-        "deposit_pickup_point_id": None,
+        "deposit_pickup_id": None,
         "recipient_id": "pe01",
-        "withdrawal_pickup_point_id": None,
-        "status": ShippingStatus.CANCELLED,
+        "withdrawal_pickup_id": None,
+        "status": ShipmentStatus.CANCELLED,
         "updated_at": datetime.fromisoformat("2025-12-04T18:00:00"),
     },
 }
